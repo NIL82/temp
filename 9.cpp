b330bed285b8299ef493fa13629b4e58da8817d6
NIL82 <nile4rock@gmail.com>
2017-04-26 00:12:38 +0530

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -1,213 +1,218 @@
-#include <iostream>
-#include <string.h>
+#include<iostream>				//Header files
+#include<stdlib.h>
+#include<string.h>
 using namespace std;
-#define MAX 10
 
-struct hash {
-    char name[20];
-    long int tele;
-    int chain;
-}rec[MAX];
-
-class hashTable {
+class node{				//Class for element of an array
 public:
-    hashTable() {
-        for(int i=0;i<MAX;i++) {
-            strcpy(rec[i].name,"-");
-            rec[i].tele = 0;
-            rec[i].chain = -1;
-        }
-    }
-
-    int hashing(long int tel) {
-        return (tel%MAX);
-    }
-    void WithReplacement(struct hash h);
-    void WithoutReplacement(struct hash h);
-    void disp();
-
+	int key;
+	int tel;
+	char name[20];
+	int chain;
+	node()
+	{
+		key=-1;
+		tel=-1;
+		chain=-1;
+		name[0]='\0';
+	}
 };
 
-void hashTable::WithoutReplacement(struct hash h) {
-
-		int k = hashing(h.tele);
-	    int p = k;
-	    int i;
-
-	    if(rec[k].tele == 0) {
-	        rec[k].tele = h.tele;
-	        strcpy(rec[k].name, h.name);
-	    }
-	    else {
-	       i = k+1;              // i points to next record
-	       if(i >= MAX)          // to move to first record if i is greater than 9
-	    	   i = i%MAX;
-	       do
-	       {
-
-	            if(rec[i].tele == 0)
-	            {
-	                rec[i].tele = h.tele;
-	                strcpy(rec[i].name, h.name);
-	                int j=k;
-										do
-										{
-											if(hashing(rec[j].tele) == k && rec[j].chain == -1)
-											{
-												rec[j].chain = i;
-												break;
-											}
-											j++;
-											if(j >= MAX)
-												j = j%MAX;
-										}while(j%MAX != k);
-	                break;
-	            }
-
-	            i++;
-
-	        }while(i%MAX!=k);
-
-	       if(i == k)
-	       {
-	            cout<<"\nSorry, could'nt insert new record. Hash Table is full... ";
-	        }
-
-	    }
+class hash{						//Class for hashing
+	node arr[10];
+	char name1[20];
+	int tel1;
+	int key1;
+public:
+	hash()						//Default constructor
+	{
+		name1[0]='\0';
+		tel1=-1;
+		key1=-1;
+	}
+	void accept();				//Member Functions
+	void display();
+	void replacement();
+	void wreplacement();
+	void swap1(node *,node *);
+};
+void hash::accept(){
+	cout<<"Enter the name of student"<<endl;
+	cin>>name1;
+	cout<<"Enter the n telephone number"<<endl;
+	cin>>tel1;
 }
+void hash::wreplacement(){//Hashing without replacement
+	int i=0;
+	int flag=0;
+	key1=tel1%10;
+	int n=tel1%10;
+	int n1=1;
+	int key2;
 
-void hashTable::disp() {
-    cout<<"\n\n";
-    cout<<"\n\tINDEX\tNAME\tTELEPHONE\tCHAIN";
-    for(int i=0; i<MAX; i++) {
-        if(rec[i].tele == 0) {
-            cout<<"\n\t  "<<i<<"\t"<<rec[i].name<<"\t"<<rec[i].tele<<"\t"<<rec[i].chain;
-        } else {
-        cout<<"\n\t  "<<i<<"\t"<<rec[i].name<<"\t"<<rec[i].tele<<"\t"<<rec[i].chain;
-        }
-    }
+	while(arr[key1].key!=-1)
+	{
+		if(key1==9)
+			key1=0;
+		key1=(tel1+i)%10;
+		i++;
+		flag=1;
+	}
+	if(i>9)
+	{
+		cout<<"The chain is full"<<endl;
+		return;
+	}
+
+	key2=key1;
+	arr[key1].key=key1;
+	if(flag==0)
+	{
+		strcpy(arr[key1].name,name1);
+		arr[key1].tel=tel1;
+		arr[key1].chain=-1;
+	}
+	else
+	{
+		arr[key2].tel=tel1;
+		strcpy(arr[key2].name,name1);
+		if(key1==0)
+			key1=10;
+		while(n!=(arr[--key1].tel)%10 && n1!=10)
+		{
+			if(key1==0)
+			{
+				key1=10;
+			}
+			n1++;
+		}
+		if(n1==10)
+		{
+			arr[n+1].chain=-1;
+		}
+		else
+		{
+			arr[key1].chain=key2;
+		}
+	}
+	display();
 }
+void hash::replacement(){		//Hashing with replacement
+	int i=0;
+	int flag=0;
+	key1=tel1%10;
+	int n=tel1%10;
+	int n1=1;
+	int key2;
 
-void hashTable::WithReplacement(struct hash h)
+	while(arr[key1].key!=-1)
+	{
+		if(key1==9)
+			key1=0;
+		key1=(tel1+i)%10;
+		i++;
+		flag=1;
+	}
+	if(i>9)
+	{
+		cout<<"The chain is full"<<endl;
+		return;
+	}
+	key2=key1;
+	arr[key1].key=key1;
+	if(key1==0)
+		key1=10;
+	if(flag==0)
+	{
+		strcpy(arr[key1].name,name1);
+		arr[key1].tel=tel1;
+		arr[key1].chain=-1;
+	}
+	else
+	{
+		arr[key2].tel=tel1;
+		strcpy(arr[key2].name,name1);
+		while(n!=(arr[--key1].tel)%10 && n1!=10)
+		{
+			if(key1==0)
+				key1=10;
+			n1++;
+		}
+		if(n1==10)
+		{
+			arr[n+1].chain=-1;
+		}
+		else
+		{
+			arr[key1].chain=key2;
+		}
+	}
+	n=tel1%10;
+	key1=key2;
+
+	if((arr[n].tel)%10!=n)
+	{
+		swap1(&arr[n],&arr[key1]);
+		while((arr[key2].tel)%10!=(arr[--key1].tel)%10)
+		{
+			if(key1==0 || key1==-1)
+				key1=10;
+		}
+		arr[key1].chain=arr[key2].key;
+	}
+	display();
+}
+void hash::swap1(node *a,node *b)
+//Swapping elements in array
 {
-    int k = hashing(h.tele);
-    int p = k;
-    int i, change_pos;
-    if(rec[k].tele == 0)
-    {
-        rec[k].tele = h.tele;
-        strcpy(rec[k].name, h.name);
-    }
-    else if(hashing(rec[k].tele) == hashing(h.tele))
-    {
-       i = k+1;
-       if(i >= MAX)
-    	   i = i%MAX;
-       do
-       {
-            if(hashing(rec[i].tele) == k)
-            {
-                p = k;
-            }
-            if(rec[i].tele == 0)
-            {
-                rec[i].tele = h.tele;
-                strcpy(rec[i].name, h.name);
-                int j=k;
-                do
-                {
-					if(hashing(rec[j].tele) == k && rec[j].chain == -1)
-					{
-						rec[j].chain = i;
-						break;
-					}
-					j++;
-					if(j >= MAX)
-						j = j%MAX;
-                }while(j%MAX != k);
-
-                break;
-            }
-            i++;
-        }while(i%MAX!=k);
-        if(i == k)
-        {
-            cout<<"\nSorry, could'nt insert new record. Hash Table is full... ";
-        }
-    }//else part is different
-    else
-    {
-        hash temp;
-        temp.tele = rec[k].tele;
-        strcpy(temp.name,rec[k].name);
-        temp.chain = rec[k].chain;
-        rec[k].tele = h.tele;
-        rec[k].chain = -1;
-        strcpy(rec[k].name, h.name);
-
-        i = k + 1;
-        if(i >= MAX)
-        	i = i%MAX;
-        do
-        {
-            if(rec[i].tele == 0)
-            {
-                rec[i].tele = temp.tele;
-                strcpy(rec[i].name, temp.name);
-
-                rec[i].chain = temp.chain;
-                change_pos = i;
-                break;
-            }
-            i++;
-        } while(i%MAX != k);
-
-        for(i=0;i<MAX;i++)
-        {
-            if(rec[i].chain == k)
-            {
-            	if(change_pos >= MAX)
-            		change_pos = change_pos-MAX;
-                rec[i].chain = change_pos;
-                break;
-            }
-        }
-    }
+	node *temp=new node();
+	temp->tel=a->tel;
+	a->tel=b->tel;
+	b->tel=temp->tel;
+	temp->chain=a->chain;
+	a->chain=b->chain;
+	b->chain=temp->chain;
+	strcpy(temp->name,a->name);
+	strcpy(a->name,b->name);
+	strcpy(b->name,temp->name);
 }
-
-int main()
+void hash::display(){
+	int i;
+	cout<<"Key\t"<<"Name \t"<<"Tel No\t"<<"Chain\t"<<endl;
+	for(i=0;i<10;i++)
+	{
+		cout<<i<<"\t";
+		cout<<arr[i].name<<"\t";
+		cout<<arr[i].tel<<"\t";
+		cout<<arr[i].chain<<"\t";
+		cout<<endl;
+	}
+}
+int main()					//main function
 {
-    int ch, c=1;
-    hashTable ht;
-    hash h;
-
-    do
-    {
-        cout<<"\n\n1. without replacement \n2.with replacement " ;
-        cin>>ch;
-
-        switch(ch)
-        {
-
-            case 1:
-                cout<<"\nEnter name :\t";
-                cin>>h.name;
-                cout<<"\nEnter telephone number :\t";
-                cin>>h.tele;
-                ht.WithoutReplacement(h);
-                ht.disp();
-                break;
-
-            case 2:
-                cout<<"\nEnter name :\t";
-                cin>>h.name;
-                cout<<"\nEnter telephone number :\t";
-                cin>>h.tele;
-                ht.WithReplacement(h);
-                ht.disp();
-                break;
-        }
-    }while(c==1);
-
-    return 0;
+	hash h;					//object of class
+	int ch;
+	do
+	{
+		cout<<"Menu"<<endl;
+		cout<<"1.Chaining Without Replacement"<<endl;
+		cout<<"2.Chaining With Replacement"<<endl;
+		cout<<"3.Display"<<endl;
+		cout<<"4.Exit"<<endl;
+		cin>>ch;
+		switch(ch)
+		{
+		case 1:h.accept();
+			   h.wreplacement();
+		break;
+		case 2:h.accept();
+			   h.replacement();
+		break;
+		case 3:h.display();
+		break;
+		case 4:exit(0);
+		break;
+		}
+	}while(ch<=4);
 }
+
