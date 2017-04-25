d00dca23a768c296c7bbe277457e45d7d0566672
NIL82 <nile4rock@gmail.com>
2017-04-26 00:09:06 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,95 +1,125 @@
 #include<iostream>
-#include<stdlib.h>
-int visited[10];
+#include<stdlib.h>;
+#define INF 999
+#define MAX 10
 using namespace std;
 
-class Graph {
-    int edges,min_cost;
-    int min;
-    int a,b,u,v,n,i,j,arr[20][20];
-    string arr1[10];
-public:
-    Graph() {
-        edges=1;
-        min_cost=0;
-    }
-    void ent_node()
-    {
-        cout<<"\nEnter no of Cities";
-        cin>>n;
-        cout<<"\nEnter City names:";
-        for(i=0;i<n;i++)
-        {
-            cin>>arr1[i];
-        }
-    }
-    void kruskal(int arr[20][20],int n);
-    void read()
-    {
-        ent_node();
-        //cout<<"\nEnter the no of vertices: "<<endl;
-       // cin>>n;
-        cout<<"\nEnter the distance between cities in kms : ";
-        for(i=0;i<n;i++)
-        {
-            for(j=i+1;j<n;j++)
-            {
-                cin>>arr[i][j];
-                arr[j][i]=arr[i][j];
-                if(arr[i][j]==0)
-                    arr[i][j]=arr[j][i]=999;
-           	}
-        }
-        for(i=0;i<n;i++)                // for initialing diagonal with 0
-            for(j=0;j<n;j++)
-                if(i==j)
-            		arr[i][j]=0;
-
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                cout<<"\t\t"<<arr[i][j];
-            }
-            cout<<"\n";
-        }
-        kruskal(arr, n);
-    }
-};
-
-void Graph::kruskal(int arr[20][20],int n)
+class Graph
 {
-    while(edges<n)
-    {
-        min=999;
+	string names[MAX];
+	int nodes, edges,cost[MAX][MAX];
 
-        for(i=0;i<n;i++)
-        {
-            for(j=i+1;j<n;j++)
-            {
-                if(arr[i][j]<min)
-                {
-                    min=arr[i][j];
-                    a=u=i;
-                    b=v=j;
-                }
-            }
-        }
+public:
+	Graph()
+	{
+		nodes=0;
+		for(int i=0; i<MAX; i++)
+		{
+			for(int j=0; j<MAX; j++)
+			{
+				if(i==j)
+					cost[i][j] = 0;
+				else
+					cost[i][j] = INF;
+			}
+		}
+	}
+	void accept();
+	void display();
+	void kruskal();
+	int position(string);
+};
+void Graph::accept()
+{
+	int i, j,k, wt;
+	string start, end;
+	cout<<"\nEnter no. of vertex : ";
+	cin>>nodes;
+	cout<<"\nEnter names of vertex: ";
+	for(int i=0; i<nodes; i++)
+	{
+		cin>>names[i];
+	}
+	cout<<"\nEnter no. of edges: ";
+	cin>>edges;
+	for(i=0; i<edges; i++)
+	{
+		cout<<"\nEnter source destination and weight: ";
+		cin>>start>>end;
+		cout<<"\nEnter weight: ";
+		cin>>wt;
 
-    if(u!=v)
-    {
-        edges++;
-        cout<<"\nDistance between city : "<<arr1[a]<<"->"<<arr1[b]<<" = " <<min<<" kms";
-        min_cost=min_cost+min;
-    }
-    arr[a][b]=arr[b][a]=999;
+		j = position(start);
+		k = position(end);
 
-    }
-    cout<<"\nMinimum cost : "<<min_cost;
+		cost[j][k] = wt;
+		cost[k][j] = wt;
+	}
 }
+int Graph::position(string s)
+{
+	int i=0;
+	for(i=0; i<nodes; i++)
+		if(names[i] == s)
+			return i;
+
+	cout<<"!Vertex not found!";
+}
+void Graph::display()
+{
+	int i, j;
+	for(i=0; i<nodes; i++)
+		cout<<"\t"<<names[i];
+	for(i=0; i<nodes; i++)
+	{
+		cout<<"\n"<<names[i];
+		for(j=0; j<nodes; j++)
+				cout<<"\t"<<cost[i][j];
+	}
+}
+void Graph::kruskal()
+{
+	int mincost = 0;
+	int n = nodes;
+	edges = 1;
+	int min, i, j, u, v, a, b;
+	while(edges<n)
+	{
+		min=999;
+
+		for(i=0;i<n;i++)
+		{
+			for(j=i;j<n;j++)
+			{
+				if(cost[i][j]<min)
+				{
+					min=cost[i][j];
+					a=u=i;
+					b=v=j;
+				}
+			}
+		}
+
+		if(u!=v)
+		{
+			edges++;
+			cout<<"\nDistance between city : "<<names[a]<<"->"<<names[b]<<" = " <<min<<" kms";
+			mincost=mincost+min;
+		}
+		cost[a][b]=cost[b][a]=999;
+	}
+	cout<<"\nMinimum cost : "<<mincost;
+}
+
 
 int main()
 {
-    Graph G;
-    G.read();
+	Graph g;
+	int cost;
+
+	g.accept();
+	g.display();
+	g.kruskal();
+	return 0;
 }
+
